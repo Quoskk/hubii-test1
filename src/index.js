ea38e43d32d1425ef2d2b9f08966fd9665191391
lucasmiraglia <lucas.miraglia@coinfabrik.com>
2017-11-29 17:31:03 -0300

    change name AppBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+injectTapEventPlugin ();
+
+
+
+class App extends Component {
+	render (){
+		return (
+
+				<MuiThemeProvider>
+						<AppBar 
+							title = "Hubii"
+							iconClassNameRight="muidocs-icon-navigation-expand-more"
+						/>
+					</MuiThemeProvider>
+			);
+	};
+};
+
+
+	ReactDOM.render(<App />, document.getElementById('root'));
